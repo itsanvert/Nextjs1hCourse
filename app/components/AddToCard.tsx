97c7931da4996a2ca3e18ba1f56e3eca91f01a17
San Vert <itsanvet@gmail.com>
2024-08-15 13:27:43 +0700

    first commit

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const AddToCard = () => {
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => console.log("Click")}>
+        Add Card
+      </button>
+    </div>
+  );
+};
+
+export default AddToCard;
